f38bd99dafde4e4796e278a4c8ea4f9695705092
limingze.jiayou <limingze.jiayou@bytedance.com>
2024-08-01 16:06:36 +0800

    lsf.h: update openlava version in lsid/commands -V.
    update version to openlava_bd project 2.0.1
    
    Signed-off-by: limingze.jiayou <limingze.jiayou@bytedance.com>

--- lsf/lsf.h
+++ lsf/lsf.h
@@ -61,7 +61,7 @@ typedef long long int LS_LONG_INT;
 typedef unsigned long long LS_UNS_LONG_INT;
 
 #define LS_LONG_FORMAT ("%lld")
-#define _OPENLAVA_PROJECT_ "openlava project 2.0-1.b"
+#define _OPENLAVA_PROJECT_ "openlava_bd project 2.0.1"
 
 /* This is our identifier printed out by all daemons
  * and commands.
