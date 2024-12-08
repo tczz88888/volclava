2060560e5cf4ef4a9e3cac22730d9152f00c3d62
qinan.cn <qinan.cn@bytedance.com>
2024-12-08 14:08:18 +0800

    make install script support hostlist and startup service, and make uninstall more solid

--- volcuninstall.sh
+++ volcuninstall.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 
+# Copyright (C) 2021-2024 Bytedance Ltd. and/or its affiliates
+
 if [ $# -ne 1 ]; then
     echo "Usage: volcuninstall.sh /volclava_top"
     exit 1
@@ -16,6 +18,12 @@ if [ ! -d $1 ]; then
 fi
 
 service volclava stop
+
+daemon_pids=$(ps -ef | grep "$1/sbin" | grep -v grep | awk '{print $2}')
+if [ ! -z $daemon_pids ]; then
+    ps -ef | grep "$1/sbin" | grep -v grep | awk '{print $2}' | xargs kill -9
+fi
+
 chkconfig volclava off
 chkconfig --del volclava
 
@@ -23,7 +31,8 @@ if rpm -qa | grep volclava-1.0* > /dev/null 2>&1; then
     rpm -e volclava-1.0*
 fi
 
-rm -f /etc/init.d/volclava
+rm -f /etc/init.d/volclava*
 rm -f /etc/profile.d/volclava.*
 rm -rf $1
+
 echo "volclava has been uninstalled."
